3dceb50f3129d39e9e6e23bd35edab43095363f2
Thraix <thraix@hotmail.com>
2019-01-04 18:39:44 +0100

    Add termite and fix upp polybar look

--- setupUbuntu.sh
+++ setupUbuntu.sh
@@ -59,6 +59,19 @@ cd i3lock-fancy
 sudo make install
 cd ..
 
+echo Installing Termite
+git clone --recursive https://github.com/thestinger/termite.git
+git clone https://github.com/thestinger/vte-ng.git
+
+cd vte-ng && ./autogen.sh && make && sudo make install
+cd ../termite && make && sudo make install
+sudo ldconfig
+sudo mkdir -p /lib/terminfo/x; sudo ln -s \
+/usr/local/share/terminfo/x/xterm-termite \
+/lib/terminfo/x/xterm-termite
+
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite 60
+cd ..
 
 # install configs and scripts
 echo Installing configs and scripts
